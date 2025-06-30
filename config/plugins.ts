ec85afa6f8b76f4c80b797e85d4280b392b2b491
Seba <seba@deportv.ar>
2025-06-29 21:38:51 -0300

    add jwtsecret to plugins.ts

--- config/plugins.ts
+++ config/plugins.ts
@@ -4,10 +4,15 @@ export default ({ env }) => ({
     config: {
       provider: 'local',
       providerOptions: {
-        sizeLimit: 100000000, // 10 MB — podés subirlo a 20 MB si usás GIFs
+        sizeLimit: 100000000, // 100 MB
         uploadPath: 'public/uploads',
       },
       mimeTypes: ['image/jpeg', 'image/png', 'image/gif', 'video/mp4'],
+    },
+  },
+  'users-permissions': {
+    config: {
+      jwtSecret: env('JWT_SECRET'),
     },
   },
 });
