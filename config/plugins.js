11b971f749b1a955cfad855206b26da95ddae91f
Seba <seba@deportv.ar>
2025-07-06 14:24:02 -0300

    plugins

--- config/plugins.js
+++ config/plugins.js
@@ -1,18 +1,16 @@
-// config/plugins.ts
-export default ({ env }) => ({
+module.exports = ({ env }) => ({
   upload: {
     config: {
       provider: 'cloudinary',
       providerOptions: {
         cloud_name: env('CLOUDINARY_NAME'),
         api_key: env('CLOUDINARY_KEY'),
-        api_secret: env('CLOUDINARY_SECRET')
+        api_secret: env('CLOUDINARY_SECRET'),
       },
-    },
-  },
-  'users-permissions': {
-    config: {
-      jwtSecret: env('JWT_SECRET'),
+      actionOptions: {
+        upload: {},
+        delete: {},
+      },
     },
   },
 });
