34dd7bc039ef6660fda76bb477eabcab0386e679
Seba <seba@deportv.ar>
2025-08-01 12:29:19 -0300

    plugins con console log

--- config/plugins.js
+++ config/plugins.js
@@ -1,33 +1,37 @@
-module.exports = ({ env }) => ({
-  upload: {
-    config: {
-      provider: 'cloudinary',
-      providerOptions: {
-        cloud_name: env('CLOUDINARY_NAME'),
-        api_key: env('CLOUDINARY_KEY'),
-        api_secret: env('CLOUDINARY_SECRET'),
-      },
-      actionOptions: {
-        upload: {
-          use_filename: true,
-          unique_filename: false,
-          overwrite: false,
-          fetch_format: 'auto',
-          transformation: [
-            { quality: '100' } // ✅ ¡Este es el que realmente funciona!
-          ]
+module.exports = ({ env }) => {
+  console.log('⚙️ Plugin Cloudinary cargado con configuración personalizada'); // 👈 ACA
+
+  return {
+    upload: {
+      config: {
+        provider: 'cloudinary',
+        providerOptions: {
+          cloud_name: env('CLOUDINARY_NAME'),
+          api_key: env('CLOUDINARY_KEY'),
+          api_secret: env('CLOUDINARY_SECRET'),
         },
-        delete: {},
+        actionOptions: {
+          upload: {
+            use_filename: true,
+            unique_filename: false,
+            overwrite: false,
+            fetch_format: 'auto',
+            transformation: [
+              { quality: '100' }
+            ]
+          },
+          delete: {},
+        },
+        mimeTypes: [
+          'image/jpeg',
+          'image/png',
+          'image/gif',
+          'image/webp',
+          'image/svg+xml',
+          'video/mp4'
+        ],
+        breakpoints: [],
       },
-      mimeTypes: [
-        'image/jpeg',
-        'image/png',
-        'image/gif',
-        'image/webp',
-        'image/svg+xml',
-        'video/mp4'
-      ],
-      breakpoints: [],
     },
-  },
-});
+  };
+};
